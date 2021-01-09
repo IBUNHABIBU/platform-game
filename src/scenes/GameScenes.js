a2d1d39de67e90fb5684928b3bb75280f4a4be15
IBUNHABIBU <ibunhabib@gmail.com>
2021-01-09 09:18:51 +0300

    adding images

--- src/scenes/GameScenes.js
+++ src/scenes/GameScenes.js
@@ -5,9 +5,13 @@ export default class GameScene extends Phaser.Scene{
     super('Game');
   }
   preload(){
-    this.load.image("logo", logoImg);
+    // this.load.image("logo", logoImg);
   }
   create(){
-    this.add.image(400, 300, 'logo');
+    this.fish1 = this.add.image(400, 300, 'fish1');
+    
+    this.add.image(300, 300, 'fish2');
+    this.add.image(200, 300, 'fish3');
+    this.add.image(450, 220, 'sample');
   }
 }
