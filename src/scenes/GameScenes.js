5f02140677e96b9b36372f451debc3df29134ee7
IBUNHABIBU <ibunhabib@gmail.com>
2021-01-09 11:01:18 +0300

    adding bg image

--- src/scenes/GameScenes.js
+++ src/scenes/GameScenes.js
@@ -1,17 +1,9 @@
 import 'phaser';
-import logoImg from "../assets/zenva_logo.png";
 export default class GameScene extends Phaser.Scene{
   constructor(){
     super('Game');
   }
-  preload(){
-    // this.load.image("logo", logoImg);
-  }
   create(){
-    this.fish1 = this.add.image(400, 300, 'fish1');
-    
-    this.add.image(300, 300, 'fish2');
-    this.add.image(200, 300, 'fish3');
-    this.add.image(450, 220, 'sample');
+    this.bg = this.add.image(240,320, 'background');
   }
 }
