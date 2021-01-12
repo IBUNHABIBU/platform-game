bcc61acc4059c1e772f19de4fa1263556ff3f999
IBUNHABIBU <ibunhabib@gmail.com>
2021-01-12 17:33:04 +0300

    changing the position of the player

--- src/scenes/GameScenes.js
+++ src/scenes/GameScenes.js
@@ -12,8 +12,9 @@ export default class GameScene extends Phaser.Scene{
   }
    makePlayer(x,y) {
      
-    this.player = this.add.image(480,283,'player');
-    fish = this.add.sprite(50, 640, 'spriteFish');
+    // this.player = this.add.image(480,283,'player');
+    const fish = this.add.sprite(x, y, 'spriteFish');
+    return fish;
    }
   update(){
     if (cursors.left.isDown)
