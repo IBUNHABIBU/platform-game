d5b8db7e960aec7c4744d495562332ce95ffbd1e
IBUNHABIBU <ibunhabib@gmail.com>
2021-01-25 14:10:51 +0300

    finishing the userinput form

--- src/scenes/UserInputScene.js
+++ src/scenes/UserInputScene.js
@@ -6,33 +6,36 @@ export default class UserInput extends Phaser.Scene {
     super('Name');
   }
   create(){
-    this.gameHeading = this.add.text(40,40, 'You are the Monster Fish !', {
+    this.gameHeading = this.add.text(config.width / 8,
+      50, 'You are the Monster Fish !', {
       fontSize: '40px',
         fill: '#ffffff',
     })
-    this.about = this.add.text(40,80,'your task is to eat all the small fish and to get rid of the big fish unless you give it a shock',{
+    this.about = this.add.text( config.width / 16,
+      120,'your task is to eat all the small fish and to get rid of the big fish unless you give it a shock',{
       fontSize: '20px',
         fill: '#ffffff',
     })
-    this.input = this.add.dom(config.width/2,40, 'input', 
+    this.input = this.add.dom(config.width/2,config.height/3, 'input', 
      'background-color: #fff; width: 250px; height: 30px; font: 16px Times New Roman',
     )
     
-    this.button = this.add.dom(120,140, 'button', 
+    this.button = this.add.dom(config.width/2,config.height/2, 'button', 
      'background-color: #008cba; color: #fff; width: 250px; height: 30px; font: 16px Times New Roman; border:none;border-radius: 10px',
      'Start',
     )
     
-    // const button = document.querySelector('button');
-    // button.setAttribute('type', 'submit');
+     const button = document.querySelector('button');
+     
+    button.setAttribute('type', 'submit');
     
-    // button.on('click', (event) => {
-    //   if(event.target.type == 'submit') {
-    //     const name = document.querySelector('.name').value;
-    //     this.model = this.sys.game.globals.model;
-    //     this.model = name;
-    //     this.scene.start('Title');
-    //   }
-    // })
+    button.addEventListener('click', (event) => {
+      if(event.target.type == 'submit') {
+        const name = document.querySelector('input').value;
+        this.model = this.sys.game.globals.model;
+        this.model = name;
+        this.scene.start('Title');
+      }
+    })
   }
 }
