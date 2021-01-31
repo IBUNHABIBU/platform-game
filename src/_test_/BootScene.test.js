33b1eeb6d2aa4d1c99c112a42062e989a4fe73a7
IBUNHABIBU <ibunhabib@gmail.com>
2021-01-31 17:37:31 +0300

    fixing the lint errors

--- src/_test_/BootScene.test.js
+++ src/_test_/BootScene.test.js
@@ -1,8 +1,5 @@
 import BootScene from '../Scenes/BootScenes';
 
-const scene = new BootScene();
-
-
 test('BootScene is a subclass of Phaser.Scene', () => {
   expect(BootScene).toBeSubclassOf(Phaser.Scene);
 });
