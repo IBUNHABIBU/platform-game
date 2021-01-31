72df0120612cb68c7cea2c77b44ab20d15def2fa
IBUNHABIBU <ibunhabib@gmail.com>
2021-01-31 17:25:44 +0300

    fixing the errors

--- src/_test_/BootScene.test.js
+++ src/_test_/BootScene.test.js
@@ -6,14 +6,3 @@ const scene = new BootScene();
 test('BootScene is a subclass of Phaser.Scene', () => {
   expect(BootScene).toBeSubclassOf(Phaser.Scene);
 });
-// test('BootScene is of type function', () => {
-//   expect(typeof BootScene).toBe('function');
-// });
-
-// test('BootScene key name is Boot', () => {
-//   expect(scene.sys.config).toBe('Boot');
-// });
-
-// test('BootScene scene is not undefined', () => {
-//   expect(scene.sys.config).not.toBe(undefined);
-// });
