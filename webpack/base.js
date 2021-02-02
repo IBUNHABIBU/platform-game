840de65b24b2167f145e592a4b725a3720f6c305
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-02 23:46:46 +0300

    fixing the lint errors

--- webpack/base.js
+++ webpack/base.js
@@ -1,7 +1,6 @@
-const webpack = require('webpack');
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
 
 module.exports = {
   mode: 'development',
