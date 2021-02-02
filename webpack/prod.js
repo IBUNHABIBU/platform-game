6c8067602297719087d2ea50752f4dac54c58e9e
IBUNHABIBU <ibunhabib@gmail.com>
2021-02-02 23:54:00 +0300

    fixing the lint errors

--- webpack/prod.js
+++ webpack/prod.js
@@ -1,5 +1,5 @@
-const merge = require('webpack-merge');
-const TerserPlugin = require('terser-webpack-plugin');
+const merge = require('webpack-merge'); // eslint-disable-line import/no-extraneous-dependencies
+const TerserPlugin = require('terser-webpack-plugin'); // eslint-disable-line import/no-extraneous-dependencies
 const base = require('./base');
 
 module.exports = merge(base, {
